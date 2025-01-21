ea6e036f1a694669812d9bcd7d57059e07cb3946
emad-desoky <deskoemad@gmail.com>
2025-01-21 21:39:58 +0200

    ..

--- components/home-page/footer/Footer.jsx
+++ components/home-page/footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 py-16">
           {/* Logo and Social Links */}
-          <div className="space-y-8">
+          <div className="">
             <Logo />
             <SocialLinks />
           </div>
--- components/home-page/footer/Logo.jsx
+++ components/home-page/footer/Logo.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Zap } from "lucide-react";
+import { Image, Link } from "@nextui-org/react";
 
 const Logo = () => {
   return (
-    <div className="flex items-center space-x-2">
-      <Zap className="w-8 h-8 text-blue-500" />
-
-      <span className="text-2xl font-bold text-white">Company</span>
+    <div className="">
+      <Link href="/">
+        <Image src="/logo.png" alt="Logo" width={200} height={200} />
+      </Link>
     </div>
   );
 };
